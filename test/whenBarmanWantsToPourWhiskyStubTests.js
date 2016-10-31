62237ca1855b1ea432d7304ade3d9fd9fd9b4988
Programmist <717720@list.ru>
2016-10-31 09:43:14 +0900

    commit

--- test/whenBarmanWantsToPourWhiskyStubTests.js
+++ test/whenBarmanWantsToPourWhiskyStubTests.js
@@ -7,6 +7,23 @@ suite('Stub: when client ask 200 grams of whisky', function () {
 
     suite('barman has enough', function () {
 
+        test('barman pour 200 grams of whisky', function() {
+
+            let barmanStub = {
+                pour: function() {
+                    return 200;
+                }
+            };
+            let clientStub = {};
+            let drinkName = 'whisky';
+
+            let clientAskVolume = 200;
+
+            let volumeInGlass = barmanStub.pour(drinkName, clientAskVolume, clientStub);
+
+            assert.equal(clientAskVolume, volumeInGlass);
+        });
+
     });
 
     suite('no whisky in bar', function () {
