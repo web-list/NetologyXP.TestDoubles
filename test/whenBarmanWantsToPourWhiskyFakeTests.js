68d768cc42b76c47d8fe63e3d4c146e176cec47c
Programmist <717720@list.ru>
2016-10-31 10:14:28 +0900

    commit

--- test/whenBarmanWantsToPourWhiskyFakeTests.js
+++ test/whenBarmanWantsToPourWhiskyFakeTests.js
@@ -6,11 +6,42 @@ import { Barman } from '../src/barmen'
 import { Client} from '../src/client'
 
 
-suite('Stub: when client ask 200 grams of whisky', function () {
+suite('Fake: when client ask 200 grams of whisky', function () {
 
 
     suite('barman has enough', function () {
 
+        test("client is drunken", function() {
+
+            let barmanStub = {
+              pour: function() {
+                  return 200;
+              }
+            };
+
+            let clientFake = (function () {
+
+                let drunkVolume = 0;
+                let drink = function (volume) {
+                    drunkVolume += volume;
+                };
+
+                let isDrunken = function () {
+                    return (drunkVolume >= 200);
+                };
+
+                return {
+                    drunkVolume: drunkVolume,
+                    drink: drink,
+                    isDrunken: isDrunken
+                }
+            })();
+
+            let volumeInGlass = barmanStub.pour();
+            clientFake.drink(volumeInGlass);
+
+            assert.equal(clientFake.isDrunken(), true);
+        });
 
     });
 
